c30533628c044a11c7ab0e8b4086035a1498bada
BiscuitRulez <gaomond@gmail.com>
2025-08-06 17:25:17 +0700

    test all

--- backend/entity/Condition.go
+++ backend/entity/Condition.go
@@ -2,16 +2,16 @@ package entity
 
 import (
 	"time"
-
 	"gorm.io/gorm"
 )
 
 type Condition struct {
 	gorm.Model
-	DayOfWeek string
-	StartTime time.Time
-	EndTime   time.Time
 
-	UserID uint
-	User   User `gorm:"foreignKey:UserID"`
+	DayOfWeek string    `valid:"required~DayOfWeek is required."`
+	StartTime time.Time `valid:"required~StartTime is required."`
+	EndTime   time.Time `valid:"required~EndTime is required."`
+
+	UserID uint `valid:"required~UserID is required."`
+	User User `gorm:"foreignKey:UserID" valid:"-"`
 }
--- backend/entity/Schedule.go
+++ backend/entity/Schedule.go
@@ -2,20 +2,20 @@ package entity
 
 import (
 	"time"
-
 	"gorm.io/gorm"
 )
 
 type Schedule struct {
 	gorm.Model
-	NameTable     string
-	SectionNumber uint
-	DayOfWeek     string
-	StartTime     time.Time
-	EndTime       time.Time
 
-	OfferedCoursesID uint
-	OfferedCourses   OfferedCourses `gorm:"foreignKey:OfferedCoursesID"`
+	NameTable     string    `valid:"required~NameTable is required."`
+	SectionNumber uint      `valid:"required~SectionNumber is required."`
+	DayOfWeek     string    `valid:"required~DayOfWeek is required."`
+	StartTime     time.Time `valid:"required~StartTime is required."`
+	EndTime       time.Time `valid:"required~EndTime is required."`
+
+	OfferedCoursesID uint           `valid:"required~OfferedCoursesID is required."`
+	OfferedCourses OfferedCourses `gorm:"foreignKey:OfferedCoursesID" valid:"-"`
 
 	TimeFixedCourses          []TimeFixedCourses          `gorm:"foreignKey:ScheduleID"`
 	ScheduleTeachingAssistant []ScheduleTeachingAssistant `gorm:"foreignKey:ScheduleID"`
