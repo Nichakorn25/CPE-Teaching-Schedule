001b93a14f5962f9ca116eaf8c1d6c35b41532ca
Jxme_ksw <162119036+Nichakorn25@users.noreply.github.com>
2025-09-26 16:01:58 +0700

    b

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/plan/",
+  build: {
+    outDir: "C:/inetpub/plan",  // กำหนดให้ไฟล์ build ไปลงตรงนี้
+    emptyOutDir: true,          // ล้างไฟล์เก่าก่อน build ใหม่
+  },
   plugins: [react()],
-  base: '/plan/',
+  server: {
+    host: '0.0.0.0',
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8001',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '/api'),
+      },
+    },
+  },
 })
